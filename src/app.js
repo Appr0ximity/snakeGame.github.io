d9e443206c98a6e08b7ff62a6e0acbec4e8da261
Harshavardhan Shekharmurthy <hvshekharmurthy@gmail.com>
2023-09-09 17:25:46 +0530

    Score, High Score changes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,33 @@
 const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+
 
 let foodX, foodY;
 let snakeX = 5, snakeY = 10;
 let snakeBody= [];
 let velX=0, velY=0;
-let gameOver = false;
+let gameOver1 = false;
+let gameOver2 = false;
 let setIntervalID;
+let score = 0;
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
 
 const changeFoodPos = () =>{    //generating random numbers to change the position of the food
     foodY = Math.floor(Math.random()*30)+1;
     foodX = Math.floor(Math.random()*30)+1;
 }
 
-const gameOverHandler = () =>{  //every 125ms, game checks for game over situation, and if it's found true, this function is run
+const gameOverHandler1 = () =>{  //every 125ms, game checks for game over situation, and if it's found true, this function is run
     clearInterval(setIntervalID);
-    alert("Game Over! Little bro thinks he can run into a wall -_-");
+    alert("It's a brick wall lil bro. Not platform 9 3/4");
+    location.reload();
+}
+
+const gameOverHandler2 = () =>{  //every 125ms, game checks for game over situation, and if it's found true, this function is run
+    clearInterval(setIntervalID);
+    alert("I'm hungry, thus I'll eat myself.");
     location.reload();
 }
 
@@ -37,18 +50,23 @@ const changeDir = (k) =>{   //changing direction of the snake according to the i
 
 
 function initGame() {
-    if(gameOver)
-        return gameOverHandler();
+    if(gameOver1)
+        return gameOverHandler1();
+    if(gameOver2)
+        return gameOverHandler2();
 
-    let fx, fy;
     let htmlMarkup = `<div class="food bg-red-400" style="grid-area: ${foodY}/${foodX}"></div>`;
     if(snakeX === foodX && snakeY === foodY){
-        snakeBody.push([foodX, foodY]);
-        fx=foodX; fy=foodY;
         changeFoodPos();
+        snakeBody.push([foodX, foodY]);
+        score++;
+        highScore = Math.max(score, highScore);
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
     }
     
-    snakeBody[0] = [snakeX, snakeY];
+    
 
     snakeX+=velX;
     snakeY+=velY;
@@ -57,14 +75,15 @@ function initGame() {
         snakeBody[i]=snakeBody[i-1];
     }
 
-    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30)
-        gameOver = true;
+    snakeBody[0] = [snakeX, snakeY];
 
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30)
+        gameOver1 = true;
 
     for(let i=0; i<snakeBody.length; i++){
         htmlMarkup += `<div class="head bg-purple-600" style="grid-area: ${snakeBody[i][1]}/${snakeBody[i][0]}"></div>`;
-        // if(i !=0 && snakeBody[i][1] === snakeBody[0][1] && snakeBody[i][0] === snakeBody[0][0])
-        //     gameOver=true;
+        if(i != 0 && snakeBody[i][1] === snakeBody[0][1] && snakeBody[i][0] === snakeBody[0][0])
+            gameOver2=true;
     }
 
     playBoard.innerHTML = htmlMarkup;
@@ -72,5 +91,5 @@ function initGame() {
 }
 
 changeFoodPos();
-setIntervalID = setInterval(initGame, 100);
+setIntervalID = setInterval(initGame, 125);
 document.addEventListener("keyup", changeDir);
